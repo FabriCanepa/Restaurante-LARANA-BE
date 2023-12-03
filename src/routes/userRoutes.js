f04c3d6b7dc874137499b84b173bce496055292c
Alexis Mauricio Gunsett <mau_gunsett@hotmail.com>
2023-12-02 22:38:36 -0300

    agrego esquemas para usuarios y middleware de validación

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -6,12 +6,18 @@ import {
   postUser,
   putUser,
 } from '../controllers/userController.js';
+import { validateBody } from '../middlewares/validatebody.js';
+import { post_userSchema } from '../helpers/validationSchemas/userSchemas.js';
 
 const router = express.Router();
 
 router.get('/', getUsers);
 
-router.post('/', postUser);
+router.post(
+  '/',
+  (req, res, next) => validateBody(req, res, next, post_userSchema),
+  postUser,
+);
 
 router.put('/:id', putUser);
 
